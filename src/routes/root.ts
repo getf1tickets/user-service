bd5959b6738fd7c20766f135ae35130ebd1026ce
iverly <iverly.dev@gmail.com>
2022-01-10 12:45:04 +0100

    feat: add get user route

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,7 +1,28 @@
 import { FastifyPluginAsync } from 'fastify';
 
 const root: FastifyPluginAsync = async (fastify): Promise<void> => {
-  fastify.get('/', async () => ({ root: true }));
+  fastify.route({
+    method: 'GET',
+    url: '/:id',
+    preHandler: [
+      fastify.authentication.authorize(),
+      fastify.middlewares.useUser(),
+    ],
+    schema: {
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            id: { type: 'string' },
+            email: { type: 'string' },
+          },
+        },
+      },
+    },
+    handler: async (request, reply) => {
+      reply.send(request.user?.toJSON());
+    },
+  });
 };
 
 export default root;
